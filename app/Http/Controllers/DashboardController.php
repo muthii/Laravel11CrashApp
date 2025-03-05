333a4091d6c66d7cc498838369b438cb46abc5ad
Anthony Muthii <anthony.muthii@nufaikasacco.local>
2025-03-05 16:26:29 +0300

    Add login routes to web.php

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class DashboardControlller extends Controller
+class DashboardController extends Controller
 {
     public function index(){
         return view('users.dashboard');
--- routes/web.php
+++ routes/web.php
@@ -2,13 +2,29 @@
 
 use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\DashboardController;
 
 Route::view('/','posts.index')->name('home');
-Route::view('/dashboard','users.dashboard')->name('dashboard');
 
-Route::view('/register', 'auth.register')->name('register');
-Route::view('/login', 'auth.login')->name('login');
 
-Route::post('/register', [AuthController::class, 'register'])->name('register');
-Route::post('/login', [AuthController::class, 'login'])->name('login');
-Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+// Routes for authenticated users
+Route::middleware('auth')->group(function () {
+    // User Dashboard Route
+    Route::get('/dashboard', [DashboardController::class, 'index'])->middleware('verified')->name('dashboard');
+
+    // Logout Route
+    Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+
+});
+
+// Routes for guest users
+Route::middleware('guest')->group(function () {
+    // Register Routes
+    Route::view('/register', 'auth.register')->name('register');
+    Route::post('/register', [AuthController::class, 'register']);
+
+    // Login Routes
+    Route::view('/login', 'auth.login')->name('login');
+    Route::post('/login', [AuthController::class, 'login']);
+
+});
