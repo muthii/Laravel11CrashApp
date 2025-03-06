81623f0029236c281d14c1433efa4e730f013f74
Anthony Muthii <anthony.muthii@nufaikasacco.local>
2025-03-06 15:50:22 +0300

    Add PostFactory and PostSeeder, update DatabaseSeeder to seed posts

--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -17,7 +17,9 @@ class PostFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'user_id' => \App\Models\User::factory(),
+            'title' => $this->faker->sentence,
+            'body' => $this->faker->paragraph,
         ];
     }
 }
