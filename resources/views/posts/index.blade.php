2b3688729fbb6b5692431e247d46db2d0e67a772
Anthony Muthii <anthony.muthii@nufaikasacco.local>
2025-03-07 14:37:55 +0300

    Update PostController and index view to display latest posts with pagination

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,3 +1,28 @@
 <x-layout>
    <h1 class="title">Latest Posts</h1>
+    <div class="grid grid-cols-2 gap-6">
+    @foreach ($posts as $post)
+        <div class="mb-4 p-4 bg-white shadow rounded-lg">
+            {{-- title --}}
+            <h2 class="text-xl font-semibold">{{ $post->title }}</h2>
+
+            {{-- created by and time since creation --}}
+            <span>
+                Posted  {{ $post->created_at->diffForHumans() }}
+               by
+               <a href="" class="text-blue-500 font-medium">
+                {{ $post->user_id }}
+               </a>
+            </span>
+
+            {{-- body --}}
+            <p class="text-gray-700">{{ Str::words($post->body, 15) }}</p>
+
+        </div>
+    @endforeach
+</div>
+<div class="mt-8">
+    {{ $posts->links() }}
+</div>
+
 </x-layout>
